055bd8d6b6c60cdaf56b9759dd26e381f1fe78ec
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-07-24 20:34:10 -0300

    Source code improved

--- Program.cs
+++ Program.cs
@@ -3,11 +3,13 @@ string datanascinput=Console.ReadLine();
 DateTime datanasc;
 if(DateTime.TryParse(datanascinput,out datanasc))
 {
-    int dia=datanasc.Day;
-    int mes=datanasc.Month;
-    int ano=datanasc.Year;
-    int idade2050=2050-ano;
-    Console.WriteLine($"Você na data do dia 23/10/2050 às 14 horas terá {idade2050} anos.");
+    DateTime data2050=new DateTime(2050,10,23,14,0,0);
+    int idade2050=data2050.Year-datanasc.Year;
+    if(datanasc>data2050.AddYears(-idade2050))
+    {
+        idade2050--;
+    }
+    Console.WriteLine($"Você na data do dia {data2050.ToString("dd/MM/yyyy")} às 14 horas terá {idade2050} anos.");
 }
 else
 {
