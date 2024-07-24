715e37c657ce1d30e3a8bc9bc5c3b64c38ab3647
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-07-24 20:00:52 -0300

    Initial release

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Digite sua data de nascimento:");
+string datanascinput=Console.ReadLine();
+DateTime datanasc;
+if(DateTime.TryParse(datanascinput,out datanasc))
+{
+    int dia=datanasc.Day;
+    int mes=datanasc.Month;
+    int ano=datanasc.Year;
+    int idade2050=2050-ano;
+    Console.WriteLine($"Você na data do dia 23/10/2050 às 14 horas terá {idade2050} anos.");
+}
+else
+{
+    Console.WriteLine("Data inserida incorretamente.");
+    return;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+DESAFIO
+
+Quantos anos você terá no dia 23/10/2050 às 14hs.
